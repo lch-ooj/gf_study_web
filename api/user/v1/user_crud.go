622151091302839c728c8ef905f35d7214bd61a3
lch-ooj <1721871912@qq.com>
2025-12-02 22:02:05 +0800

    add customer_sign_in function

--- api/user/v1/user_crud.go
+++ api/user/v1/user_crud.go
@@ -4,53 +4,63 @@ import "github.com/gogf/gf/v2/frame/g"
 
 // ========== 外卖用户列表 ==========
 type CustomerListReq struct {
-    g.Meta `path:"/customer/list" method:"get" tags:"外卖用户管理" summary:"获取外卖用户列表"`
+	g.Meta `path:"/customer/list" method:"get" tags:"外卖用户管理" summary:"获取外卖用户列表"`
 }
 type CustomerListRes struct {
-    List interface{} `json:"list" dc:"用户列表"`
+	List interface{} `json:"list" dc:"用户列表"`
 }
 
 // ========== 创建外卖用户 ==========
 type CustomerCreateReq struct {
-    g.Meta   `path:"/customer/create" method:"post" tags:"外卖用户管理" summary:"创建外卖用户"`
-    Name     string `json:"name" v:"required#用户名不能为空" dc:"用户名"`
-    Password string `json:"password" v:"required#密码不能为空" dc:"密码"`
-    Phone    string `json:"phone" dc:"手机号"`
-    Address  string `json:"address" dc:"地址"`
+	g.Meta   `path:"/customer/create" method:"post" tags:"外卖用户管理" summary:"创建外卖用户"`
+	Name     string `json:"name" v:"required#用户名不能为空" dc:"用户名"`
+	Password string `json:"password" v:"required#密码不能为空" dc:"密码"`
+	Phone    string `json:"phone" dc:"手机号"`
+	Address  string `json:"address" dc:"地址"`
 }
 type CustomerCreateRes struct {
-    Uid int64 `json:"uid" dc:"用户ID"`
+	Uid int64 `json:"uid" dc:"用户ID"`
 }
 
 // ========== 更新外卖用户 ==========
 type CustomerUpdateReq struct {
-    g.Meta  `path:"/customer/update" method:"put" tags:"外卖用户管理" summary:"更新外卖用户"`
-    Uid     int    `json:"uid" v:"required#用户ID不能为空" dc:"用户ID"`
-    Name    string `json:"name" dc:"用户名"`
-    Phone   string `json:"phone" dc:"手机号"`
-    Address string `json:"address" dc:"地址"`
+	g.Meta  `path:"/customer/update" method:"put" tags:"外卖用户管理" summary:"更新外卖用户"`
+	Uid     int    `json:"uid" v:"required#用户ID不能为空" dc:"用户ID"`
+	Name    string `json:"name" dc:"用户名"`
+	Phone   string `json:"phone" dc:"手机号"`
+	Address string `json:"address" dc:"地址"`
 }
 type CustomerUpdateRes struct {
-    Success bool `json:"success" dc:"是否成功"`
+	Success bool `json:"success" dc:"是否成功"`
 }
 
 // ========== 删除外卖用户 ==========
 type CustomerDeleteReq struct {
-    g.Meta `path:"/customer/delete" method:"delete" tags:"外卖用户管理" summary:"删除外卖用户"`
-    Uid    int `json:"uid" v:"required#用户ID不能为空" dc:"用户ID"`
+	g.Meta `path:"/customer/delete" method:"delete" tags:"外卖用户管理" summary:"删除外卖用户"`
+	Uid    int `json:"uid" v:"required#用户ID不能为空" dc:"用户ID"`
 }
 type CustomerDeleteRes struct {
-    Success bool `json:"success" dc:"是否成功"`
+	Success bool `json:"success" dc:"是否成功"`
 }
 
 // ========== 获取单个外卖用户 ==========
 type CustomerGetReq struct {
-    g.Meta `path:"/customer/get" method:"get" tags:"外卖用户管理" summary:"获取外卖用户详情"`
-    Uid    int `json:"uid" in:"query" v:"required#用户ID不能为空" dc:"用户ID"`
+	g.Meta `path:"/customer/get" method:"get" tags:"外卖用户管理" summary:"获取外卖用户详情"`
+	Uid    int `json:"uid" in:"query" v:"required#用户ID不能为空" dc:"用户ID"`
 }
 type CustomerGetRes struct {
-    Uid     int    `json:"uid" dc:"用户ID"`
-    Name    string `json:"name" dc:"用户名"`
-    Phone   string `json:"phone" dc:"手机号"`
-    Address string `json:"address" dc:"地址"`
+	Uid     int    `json:"uid" dc:"用户ID"`
+	Name    string `json:"name" dc:"用户名"`
+	Phone   string `json:"phone" dc:"手机号"`
+	Address string `json:"address" dc:"地址"`
+}
+
+// ========== 外卖用户登录 ==========
+type CustomerSignInReq struct {
+	g.Meta   `path:"/customer/sign-in" method:"post" tags:"外卖用户管理" summary:"外卖用户登录"`
+	Name     string `json:"name" v:"required#用户名不能为空" dc:"用户名"`
+	Password string `json:"password" v:"required#密码不能为空" dc:"密码"`
+}
+type CustomerSignInRes struct {
+	Uid int64 `json:"uid" dc:"登录成功的用户ID"`
 }
--- internal/controller/user/user_v1_crud.go
+++ internal/controller/user/user_v1_crud.go
@@ -1,78 +1,100 @@
 package user
 
 import (
-    "context"
+	"context"
+	"database/sql"
 
-    v1 "gf-demo-user/api/user/v1"
-    "gf-demo-user/internal/dao"
-    "gf-demo-user/internal/model/entity"
+	"github.com/gogf/gf/v2/errors/gerror"
+
+	v1 "gf-demo-user/api/user/v1"
+	"gf-demo-user/internal/dao"
+	"gf-demo-user/internal/model/entity"
 )
 
 // CustomerList 获取外卖用户列表
 func (c *ControllerV1) CustomerList(ctx context.Context, req *v1.CustomerListReq) (res *v1.CustomerListRes, err error) {
-    var customers []entity.Customer
-    err = dao.Customer.Ctx(ctx).Scan(&customers)
-    if err != nil {
-        return nil, err
-    }
-    return &v1.CustomerListRes{List: customers}, nil
+	var customers []entity.Customer
+	err = dao.Customer.Ctx(ctx).Scan(&customers)
+	if err != nil {
+		return nil, err
+	}
+	return &v1.CustomerListRes{List: customers}, nil
 }
 
 // CustomerCreate 创建外卖用户
 func (c *ControllerV1) CustomerCreate(ctx context.Context, req *v1.CustomerCreateReq) (res *v1.CustomerCreateRes, err error) {
-    result, err := dao.Customer.Ctx(ctx).Data(map[string]interface{}{
-        "name":     req.Name,
-        "password": req.Password,
-        "phone":    req.Phone,
-        "address":  req.Address,
-    }).Insert()
-    if err != nil {
-        return nil, err
-    }
-    id, _ := result.LastInsertId()
-    return &v1.CustomerCreateRes{Uid: id}, nil
+	result, err := dao.Customer.Ctx(ctx).Data(map[string]interface{}{
+		"name":     req.Name,
+		"password": req.Password,
+		"phone":    req.Phone,
+		"address":  req.Address,
+	}).Insert()
+	if err != nil {
+		return nil, err
+	}
+	id, _ := result.LastInsertId()
+	return &v1.CustomerCreateRes{Uid: id}, nil
 }
 
 // CustomerUpdate 更新外卖用户
 func (c *ControllerV1) CustomerUpdate(ctx context.Context, req *v1.CustomerUpdateReq) (res *v1.CustomerUpdateRes, err error) {
-    data := make(map[string]interface{})
-    if req.Name != "" {
-        data["name"] = req.Name
-    }
-    if req.Phone != "" {
-        data["phone"] = req.Phone
-    }
-    if req.Address != "" {
-        data["address"] = req.Address
-    }
+	data := make(map[string]interface{})
+	if req.Name != "" {
+		data["name"] = req.Name
+	}
+	if req.Phone != "" {
+		data["phone"] = req.Phone
+	}
+	if req.Address != "" {
+		data["address"] = req.Address
+	}
 
-    _, err = dao.Customer.Ctx(ctx).Where("uid", req.Uid).Data(data).Update()
-    if err != nil {
-        return nil, err
-    }
-    return &v1.CustomerUpdateRes{Success: true}, nil
+	_, err = dao.Customer.Ctx(ctx).Where("uid", req.Uid).Data(data).Update()
+	if err != nil {
+		return nil, err
+	}
+	return &v1.CustomerUpdateRes{Success: true}, nil
 }
 
 // CustomerDelete 删除外卖用户
 func (c *ControllerV1) CustomerDelete(ctx context.Context, req *v1.CustomerDeleteReq) (res *v1.CustomerDeleteRes, err error) {
-    _, err = dao.Customer.Ctx(ctx).Where("uid", req.Uid).Delete()
-    if err != nil {
-        return nil, err
-    }
-    return &v1.CustomerDeleteRes{Success: true}, nil
+	_, err = dao.Customer.Ctx(ctx).Where("uid", req.Uid).Delete()
+	if err != nil {
+		return nil, err
+	}
+	return &v1.CustomerDeleteRes{Success: true}, nil
 }
 
 // CustomerGet 获取外卖用户详情
 func (c *ControllerV1) CustomerGet(ctx context.Context, req *v1.CustomerGetReq) (res *v1.CustomerGetRes, err error) {
-    var customer entity.Customer
-    err = dao.Customer.Ctx(ctx).Where("uid", req.Uid).Scan(&customer)
-    if err != nil {
-        return nil, err
-    }
-    return &v1.CustomerGetRes{
-        Uid:     customer.Uid,
-        Name:    customer.Name,
-        Phone:   customer.Phone,
-        Address: customer.Address,
-    }, nil
+	var customer entity.Customer
+	err = dao.Customer.Ctx(ctx).Where("uid", req.Uid).Scan(&customer)
+	if err != nil {
+		return nil, err
+	}
+	return &v1.CustomerGetRes{
+		Uid:     customer.Uid,
+		Name:    customer.Name,
+		Phone:   customer.Phone,
+		Address: customer.Address,
+	}, nil
+}
+
+// CustomerSignIn 验证外卖用户登录
+func (c *ControllerV1) CustomerSignIn(ctx context.Context, req *v1.CustomerSignInReq) (res *v1.CustomerSignInRes, err error) {
+	var customer entity.Customer
+	err = dao.Customer.Ctx(ctx).Where("name", req.Name).Where("password", req.Password).Scan(&customer)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// if customer.Uid == 0 {
+	// 	return nil, gerror.New("用户名或密码不正确")
+	// }
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, gerror.New("用户名或密码不正确")
+		}
+		return nil, err
+	}
+	return &v1.CustomerSignInRes{Uid: int64(customer.Uid)}, nil
 }
